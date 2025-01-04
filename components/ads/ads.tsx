50cc33f1d1408bc39f06c041f4de68774c3d9ff8
Lucifer472 <hardiksadhu472@gmail.com>
2025-01-04 14:31:26 +0530

    my-gujarat

---
+++ components/ads/ads.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { AdsWrapper } from "@/components/wrappers/ad-wrapper";
+
+export const Ad1 = () => {
+  return <AdsWrapper id="8749450104" />;
+};
+
+export const Ad2 = () => {
+  return <AdsWrapper id="1223100296" />;
+};
+
+export const Ad3 = () => {
+  return <AdsWrapper id="3991814076" />;
+};
+
+export const Ad4 = () => {
+  return <AdsWrapper id="9442902569" />;
+};
+
+export const Ad5 = () => {
+  return <AdsWrapper id="1209189327" />;
+};
